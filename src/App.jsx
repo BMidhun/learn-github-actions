0614848e0346ac35f91697e2299db138bddc7979
BMidhun <midhunbabygeorge@gmail.com>
2021-12-28 21:05:15 +0530

    add content to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello React!</p>
+        <p>Hello React.</p>
+        <p>This is build using vercel</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
